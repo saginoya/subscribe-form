a907b1a8a0401192941ebf97d46b509cd9eed06d
shu-saginoya <saginoya@create-nippo.co.jp>
2023-01-20 17:03:03 +0900

    fix - ElBtn component

--- src/components/ElBtn.vue
+++ src/components/ElBtn.vue
@@ -8,15 +8,36 @@
 
 <style scoped lang="scss">
 @use "@/assets/sass/variables" as v;
-@use "@/assets/sass/mixin" as m;
 
 $color: v.$co_secondary;
 $color: green !default;
+
+@mixin el_btn_base($color: #e25c00) {
+  display: inline-block;
+  width: v.$wi_btnBase;
+  max-width: 100%;
+  padding: v.$sp_2 v.$sp_1;
+  background-color: $color;
+  border: 2px solid transparent;
+  box-shadow: v.$sh_base;
+  color: v.$co_fontBaseDark;
+  font-size: v.$fo_sizeLg;
+  text-align: center;
+  text-decoration: none;
+  transition: v.$tr_speed;
+  &:focus,
+  &:hover {
+    background-color: #fff;
+    border-color: currentColor;
+    color: $color;
+    outline: 2px currentColor;
+  }
+}
 .el_btn {
-  @include m.el_btn_base($color);
+  @include el_btn_base($color);
 }
 .el_roundedBtn {
-  @include m.el_btn_base($color);
+  @include el_btn_base($color);
   border-radius: 10px;
 }
 </style>
