0a46045db347c4874230246b533090dbedadab40
shu-saginoya <saginoya@create-nippo.co.jp>
2023-01-27 15:08:12 +0900

    update - Organize long content as part components

---
+++ src/components/PartsContacts.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import BlCta from "@/components/BlCta.vue";
+
+const title: string = "お問い合わせ";
+const text: string =
+  "その他、不明な点などあれば以下のメールアドレスまでご連絡ください。";
+const email: string = "hambai@create-nippo.co.jp";
+</script>
+
+<template>
+  <BlCta :title="title">
+    <p>
+      {{ text }}
+    </p>
+    <p>
+      <a :href="`mailto:${email}`">{{ email }}</a>
+    </p>
+  </BlCta>
+</template>
---
+++ src/components/PartsNoticeList.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import BlFaq from "@/components/BlFaq.vue";
+import BlFaqA from "@/components/BlFaqA.vue";
+import BlFaqQ from "@/components/BlFaqQ.vue";
+
+interface Faq {
+  questions: string[];
+  answers: string[];
+}
+
+const faqs: Array<Faq> = [
+  {
+    questions: ["年間購読のお申し込みの流れについて"],
+    answers: [
+      "前納制となりますので、先に請求書をお送りし、ご入金確認後の発送となります。",
+    ],
+  },
+  {
+    questions: ["購読開始月について"],
+    answers: ["ご指定が無ければ、お申し込み月からの開始となります。"],
+  },
+  {
+    questions: ["その他の指定について"],
+    answers: [
+      "連絡事項などにつきましては、備考欄にご記入をお願いいたします。",
+      "例：請求先・送付先を別途ご指定など",
+    ],
+  },
+];
+</script>
+
+<template>
+  <BlFaq>
+    <template v-for="(faq, index) in faqs" :key="index">
+      <BlFaqQ
+        ><p v-for="(question, qIndex) in faq.questions" :key="index + qIndex">
+          {{ question }}
+        </p></BlFaqQ
+      >
+      <BlFaqA>
+        <p v-for="(answer, aIndex) in faq.answers" :key="index + aIndex">
+          {{ answer }}
+        </p>
+      </BlFaqA>
+    </template>
+  </BlFaq>
+</template>
