5127d4e856cbdd9f8f0d735f0b6532830f78ed08
shu-saginoya <saginoya@create-nippo.co.jp>
2023-02-17 15:07:55 +0900

    add - install axios

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import axios from "axios";
+import VueAxios from "vue-axios";
 import mdiVue from "mdi-vue/v3";
 import * as mdijs from "@mdi/js";
 
@@ -10,7 +11,7 @@ import "./assets/sass/style.scss";
 const app = createApp(App);
 
 app.use(router);
-
+app.use(VueAxios, axios);
 app.use(mdiVue, {
   icons: mdijs,
 });
