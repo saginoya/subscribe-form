832a5421cb930d3af11b57ac1d7d09de25c7259a
shu-saginoya <saginoya@create-nippo.co.jp>
2023-02-17 15:09:21 +0900

    add - ElError component

---
+++ src/components/ElError.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p class="el_error">
+    <slot></slot>
+  </p>
+</template>
+
+<style scoped lang="scss">
+@use "@/assets/sass/variables" as v;
+
+$color: v.$co_error;
+$color: red !default;
+
+.el_error {
+  color: $color;
+}
+</style>
