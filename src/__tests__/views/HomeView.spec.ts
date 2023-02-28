0c60a13448f0ef7977e82fc710c974a7e4910809
shu-saginoya <saginoya@create-nippo.co.jp>
2023-02-28 16:16:28 +0900

    Public Version 1.0.0

--- src/__tests__/views/HomeView.spec.ts
+++ src/__tests__/views/HomeView.spec.ts
@@ -3,7 +3,7 @@ import { mount } from "@vue/test-utils";
 import HomeView from "@/views/HomeView.vue";
 
 describe("HomeView.vueのテスト", () => {
-  test("入力値一覧オブジェクトを受け取っていない場合はエラーメッセージを表示", async () => {
+  test("入力値一覧オブジェクトを受け取っていない場合はエラーメッセージを表示", () => {
     const wrapper = mount(HomeView);
     const actual = wrapper.find(`[data-testid="form-body-error"]`).exists();
     const expected = true;
